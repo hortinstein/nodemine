7fcdd4fb879f909f0210051c50d09bbcb9e8c0b2
Alex Hortin <hortinstein@gmail.com>
2013-11-24 09:46:20 -0500

    fixed formatting of the files, tabs and spaces, and fixed formatting so it would work better on raspberry pi screen

--- index.js
+++ index.js
@@ -10,53 +10,51 @@ var goxData = {};
 var rateData = {};
 ////////////////////////////////////////////////////////
 mtgox.on('ticker', function(ticker){
-	goxData.high = ticker.high.display_short;
-	goxData.low = ticker.low.display_short;
+	//goxData.high = ticker.high.display_short;
+	//goxData.low = ticker.low.display_short;
 	goxData.last = ticker.last.display_short;
 	goxData.last_num = ticker.last.value;
-})
+});
 mtgox.connect('usd');
 
 ////////////////////////////////////////////////////////
 
-clear = function () {
+var clear = function () {
 	console.log('\033c');
-}
+};
 ////////////////////////////////////////////////////////
 
-calcRate = function	(hashrate) {
+var calcRate = function	(hashrate) {
 	var url = 'http://www.alloscomp.com/bitcoin/calculator/json?hashrate='+(hashrate*1000000);
 	request({url:url}, function (error, response, body) {
 		body = JSON.parse(body);
-	  	rateData.coins_per_day = (body.coins_per_hour * 24).toFixed(6);
-	  	rateData.dollars_per_day = '$'+(rateData.coins_per_day * goxData.last_num).toFixed(2);
+        rateData.coins_per_day = (body.coins_per_hour * 24).toFixed(6);
+        rateData.dollars_per_day = '$'+(rateData.coins_per_day * goxData.last_num).toFixed(2);
 		rateData.total_earned = '$'+(goxData.last_num * btcGuild.total).toFixed(2);
 	});
 
-}
+};
 ////////////////////////////////////////////////////////
-btcGUpdate = function (apiKey) {
-	var url = "https://www.btcguild.com/api.php?api_key="+config.apiKey
+var btcGUpdate = function (apiKey) {
+	var url = "https://www.btcguild.com/api.php?api_key="+config.apiKey;
 	request({url:url, json:true}, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    btcGuild.total = body.user.total_rewards;
-	    btcGuild['24h'] = body.user.past_24h_rewards;
-	    btcGuild.difficulty = body.pool.difficulty;
-	    try{
-	    	btcGuild.worker = body.workers[1].hash_rate;	
-	    	
-	    	calcRate(btcGuild.worker);
-
-	    } catch (e){
-	    	btcGuild.worker = "DOWN";
-	    }
-	    
-	  }
+        if (!error && response.statusCode == 200) {
+            btcGuild.total = body.user.total_rewards;
+            btcGuild['24h'] = body.user.past_24h_rewards;
+            btcGuild.difficulty = body.pool.difficulty;
+            try{
+                btcGuild.worker = body.workers[1].hash_rate;	
+                calcRate(btcGuild.worker);
+    
+            } catch (e){
+                btcGuild.worker = "DOWN";
+            }
+        }
 	});
-}
+};
 
 ////////////////////////////////////////////////////////
-refresh = function (){
+var refresh = function (){
 	clear();
 	//terminal.colorize('%1');
 	console.log('kintaro miner');
@@ -67,7 +65,7 @@ refresh = function (){
 	console.log(goxData);
 	//terminal.colorize('%0');
 	console.log(rateData);
-}
+};
 ////////////////////////////////////////////////////////
 setInterval(btcGUpdate,16000);
-setInterval(refresh,1000);
+setInterval(refresh,10000);

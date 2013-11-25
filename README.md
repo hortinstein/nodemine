c0509abeb584f5bb4b042b9c5a6afa82cbce584d
Alex Hortin <hortinstein@gmail.com>
2013-11-24 19:27:15 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+
+simple monitor script for my raspberry pi minepeon miner
+
+Example:
+
+```
+kintaro miner
+uptime days:13 hours:13
+rate: 4161.35 MH/S
+earning: 0.003434 BTC/day
+earned: 0.05085677 BTC
+---------------------------
+price per coin: $800.00
+$2.75/day, $40.69 total
+```
--- uptime.js
+++ uptime.js
@@ -1,13 +1,12 @@
 module.exports = uptimes;
 
-uptimed = require('uptimed');
+var uptimed = require('uptimed');
 os = require('os');
 
 function uptimes() {
 
     var hours,days,minutes,seconds;
-    var data = {};
-    var seconds= Math.round(uptimed.getUptime());
+    seconds= Math.round(uptimed.getUptime());
     days = parseInt(seconds / 86400);
     hours = parseInt((seconds % 86400) / 3600);
     minutes = parseInt((seconds % 3600) / 60);

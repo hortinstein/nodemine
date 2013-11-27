c621a31fac41e83d09253fd9c59917f06c052994
hortinstein <hortinstein@gmail.com>
2013-11-27 20:56:26 +0900

    added basic logging code, need to debug cloudant parts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var config = require('./.config.json');
-var printUptime = require('./uptime.js');
+var uptimed = require('uptimed');
+os = require('os');
 var request = require('request');
 var terminal = require('color-terminal');
 var mtgox    = require('mtgox-orderbook');
@@ -9,13 +10,33 @@ var btcGuild={};
 var goxData = {};
 var rateData = {};
 var minerLog = {};
+var uptime=0;
+
+var SEC = 1000;
+var MIN = 60 * SEC;
+var HOUR = 60 * MIN;
+var DAY = 24 * HOUR;
+
+function printUptime() {
+    var hours,days,minutes,seconds;
+    //storing complete json to sync to log
+	minerLog.uptimeSeconds = Math.round(uptimed.getUptime());
+	//
+    seconds= Math.round(uptimed.getUptime());
+    days = parseInt(seconds / 86400);
+    hours = parseInt((seconds % 86400) / 3600);
+    minutes = parseInt((seconds % 3600) / 60);
+    seconds =  seconds % 60;
+    console.log('uptime days:'+days+' hours:'+hours);//+' min:'+minutes+' sec:'+seconds);
+}
 
 ////////////////////////////////////////////////////////
 //Function that is responsible for updating the gox price
 //
 mtgox.on('ticker', function(ticker){
-	//goxData.high = ticker.high.display_short;
-	//goxData.low = ticker.low.display_short;
+	minerLog.goxData = ticker;
+	goxData.high = ticker.high.display_short;
+	goxData.low = ticker.low.display_short;
 	goxData.last = ticker.last.display_short;
 	goxData.last_num = ticker.last.value;
 });
@@ -34,10 +55,22 @@ var clear = function () {
 var calcRate = function	(hashrate) {
 	var url = 'http://www.alloscomp.com/bitcoin/calculator/json?hashrate='+(hashrate*1000000);
 	request({url:url}, function (error, response, body) {
-		body = JSON.parse(body);
-        rateData.coins_per_day = (body.coins_per_hour * 24).toFixed(6);
-        rateData.dollars_per_day = '$'+(rateData.coins_per_day * goxData.last_num).toFixed(2);
-		rateData.total_earned = '$'+(goxData.last_num * btcGuild.total).toFixed(2);
+		try{
+			body = JSON.parse(body);
+			//storing complete json to sync to log
+			minerLog.rate = body;
+			//
+	        rateData.coins_per_day = (body.coins_per_hour * 24).toFixed(6);
+	        rateData.dollars_per_day = '$'+(rateData.coins_per_day * goxData.last_num).toFixed(2);
+			rateData.total_earned = '$'+(goxData.last_num * btcGuild.total).toFixed(2);
+			//storing complete json to sync to log
+			minerLog.rateData = rateData;	
+			//
+		} catch (e) {
+			rateData = {"ERROR":"error parsing alloscomp json"};
+
+		}
+		
 	});
 
 };
@@ -46,22 +79,25 @@ var calcRate = function	(hashrate) {
 //Pulls information from BTC API
 //
 var btcGUpdate = function (apiKey) {
-	var url = "https://www.btcguild.com/api.php?api_key="+config.apiKey;
+	var url = "https://www.btcguild.com/api.php?api_key="+config.api_key;
 	request({url:url, json:true}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
+        	//storing complete json to sync to log
+			minerLog.btcGuild = body;
+        	//
             try{
                 btcGuild.total = body.user.total_rewards;
                 btcGuild['24h'] = body.user.past_24h_rewards;
                 btcGuild.difficulty = body.pool.difficulty;
             } catch (e){
-                btcGuild = {};
+                btcGuild = {"ERROR":"error parsing btcguild json"};
             }
             try{
                 btcGuild.worker = body.workers[1].hash_rate;	
                 calcRate(btcGuild.worker);
     
             } catch (e){
-                btcGuild.worker = "DOWN";
+                btcGuild.worker = "miner DOWN";
             }
         }
 	});
@@ -72,16 +108,23 @@ var btcGUpdate = function (apiKey) {
 //Function that pushes to iris couch used for logging
 //
 var pushLog = function (){
-    var rawlog = {}; //this will contain the btcGuild,goxData and rateData 
-    rawlog.milliseconds = (new Date).getTime();
-    rawlong.datestring = Date().valueOf();
-    try{
-        rawlog.btcGuild = btcGuild;
-        rawlog.goxData = goxData;
-        rawlog.rateData = rateData;
-    } catch (e){
-        
-    }
+    console.log(minerLog);
+    var couch_host = 'http://' + config.couch_host + ':' + config.couch_port
+    var nano = require('nano')(couch_host)
+      , username = config.username
+      , userpass = config.password
+      , callback = console.log // this would normally be some callback
+      , cookies  = {} // store cookies, normally redis or something
+      , database_name = config.couch_db_name;
+    var db = nano.db.use(config.couch_location);
+
+    client.insert(minerLog, (new Date).getTime(), function(e, body){
+		if (e) { 
+			console.log('ERROR LOGGING'+e);
+		} else {
+			console.log("logged successfully");
+        }
+    });
 }
 
 ////////////////////////////////////////////////////////
@@ -102,5 +145,6 @@ var refresh = function (){
 	console.log(rateData.dollars_per_day+"/day, "+rateData.total_earned+" total");
 };
 ////////////////////////////////////////////////////////
-setInterval(btcGUpdate,20000);
-setInterval(refresh,10000);
+setInterval(btcGUpdate,25*SEC);
+setInterval(refresh,10*SEC);
+//setInterval(pushLog,10*SEC);//5*MIN);
